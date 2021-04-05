9a97722c571df84300bdb12969029f753feb0d56
Lance Dang <lancedang64@gmail.com>
2021-04-05 19:39:39 +1000

    Created displayBookOnPage and getBookCard Tested out UI

--- myscript.js
+++ myscript.js
@@ -14,12 +14,35 @@ function Book(title, author, pages, isRead) {
     }
 }
 
-function addBookToLibrary(bookObject) {
-    myLibrary.push(bookObject);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 
+function displayBookOnPage(array) {
+    array.forEach(book => {
+        const bookCard = getBookCard(book);
+        bookCardsContainer.appendChild(bookCard);
+    });
+}
+
+function getBookCard(book) {
+    const bookCard = document.createElement("div"); 
+    bookCard.className = "bookCard";
+    bookCard.style.fontSize = "10px";
+    bookCard.style.borderStyle = "solid";
+    bookCard.style.borderColor = "black";
+    bookCard.style.margin = "10px";
+    bookCard.style.padding = "5px";
+
+    bookCard.textContent = book.info();
+    return bookCard;
+}
+
+const bookCardsContainer = document.getElementById("bookCardsContainer");
 const firstBook = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const secondBook = new Book("Harry Porter", "J.K. Rowling", 450, true);
 
 addBookToLibrary(firstBook);
-
+addBookToLibrary(secondBook);
+displayBookOnPage(myLibrary);
 
