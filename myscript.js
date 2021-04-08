033464138a4a135c4cd51e715bf9c9b0a37ef471
Lance Dang <lancedang64@gmail.com>
2021-04-08 16:59:14 +1000

    Formmatted code

--- myscript.js
+++ myscript.js
@@ -1,41 +1,43 @@
-'use strict'
+"use strict";
 
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-    this.info = function() {
-        let readStatus = isRead ? "has been read" : "not read yet";
-        const info = `${title}  by ${author}, ${pages} pages, ${readStatus}`;
-        return info;
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        this.info = function () {
+            let readStatus = isRead ? "has been read" : "not read yet";
+            const info = `${title}  by ${author}, ${pages} pages, ${readStatus}`;
+            return info;
+        };
     }
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+  myLibrary.push(book);
 }
 
 function displayBookOnPage(array) {
-    array.forEach(book => {
-        const bookCard = getBookCard(book);
-        bookCardsContainer.appendChild(bookCard);
-    });
+  array.forEach((book) => {
+    const bookCard = getBookCard(book);
+    bookCardsContainer.appendChild(bookCard);
+  });
 }
 
 function getBookCard(book) {
-    const bookCard = document.createElement("div"); 
-    bookCard.className = "bookCard";
-    bookCard.style.fontSize = "10px";
-    bookCard.style.borderStyle = "solid";
-    bookCard.style.borderColor = "black";
-    bookCard.style.margin = "10px";
-    bookCard.style.padding = "5px";
+  const bookCard = document.createElement("div");
+  bookCard.className = "bookCard";
+  bookCard.style.fontSize = "10px";
+  bookCard.style.borderStyle = "solid";
+  bookCard.style.borderColor = "black";
+  bookCard.style.margin = "10px";
+  bookCard.style.padding = "5px";
 
-    bookCard.textContent = book.info();
-    return bookCard;
+  bookCard.textContent = book.info();
+  return bookCard;
 }
 
 const bookCardsContainer = document.getElementById("bookCardsContainer");
@@ -45,4 +47,3 @@ const secondBook = new Book("Harry Porter", "J.K. Rowling", 450, true);
 addBookToLibrary(firstBook);
 addBookToLibrary(secondBook);
 displayBookOnPage(myLibrary);
-
