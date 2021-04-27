16d4a59d1d3b8a9cfafcddb393c54fe3546bc537
Lance Dang <lancedang64@gmail.com>
2021-04-28 01:09:14 +1000

    Refactored code to using class Library. Refactored remindUserInput

--- myscript.js
+++ myscript.js
@@ -22,6 +22,9 @@ class Library {
     const book = this.getBookFromInput();
     this.addBookToLibrary(book);
     this.addBookToDisplay(book);
+    this.updateLocalStorage();
+    submitForm.reset();
+    this.remindUserInput(book);
   };
 
   getBookFromInput = () => {
@@ -31,16 +34,20 @@ class Library {
     const readStatus = document.getElementById('read').checked
       ? 'READ'
       : 'NOT READ';
+    const book = new Book(title, author, pages, readStatus);
+    const isBookDetailFilled =
+      book.title.trim() !== '' && book.author.trim() !== '' && book.pages > 0;
+    return isBookDetailFilled ? book : this.remindUserInput(book);
+  };
 
-    const isFormFilled =
-      title.trim() !== '' && author.trim() !== '' && pages > 0;
-    if (!isFormFilled) {
-      remindUserInput(title, author, pages);
-      return;
-    } else {
-      const book = new Book(title, author, pages, readStatus);
-      return book;
-    }
+  remindUserInput = (book) => {
+    const titleReminder = document.getElementById('title-reminder');
+    const authorReminder = document.getElementById('author-reminder');
+    const pagesReminder = document.getElementById('pages-reminder');
+    const reminder = '*please fill in this field';
+    titleReminder.textContent = book.title.trim() === '' ? reminder : '';
+    authorReminder.textContent = book.author.trim() === '' ? reminder : '';
+    pagesReminder.textContent = book.pages <= 0 ? reminder : '';
   };
 
   addBookToLibrary = (book) => {
@@ -85,152 +92,165 @@ class Library {
     const title = e.target.id.slice(7);
     this.library = this.library.filter((book) => book.title != title);
     document.getElementById(title).remove();
-    updateLocalStorage();
+    this.updateLocalStorage();
   };
 
   changeReadStatus = (e) => {
     const title = e.target.id.slice(12);
     const currentReadStatus = e.target.innerText;
-    const changedReadStatus = currentReadStatus === 'READ'? 'NOT READ' : 'READ';
+    const newReadStatus = currentReadStatus === 'READ' ? 'NOT READ' : 'READ';
     const readStatusButton = document.getElementById(e.target.id);
     const bookIndex = this.library.findIndex((book) => book.title === title);
-    
-    this.library[bookIndex].readStatus = changedReadStatus;
-    readStatusButton.textContent = changedReadStatus;
-  
-    updateLocalStorage();
+
+    this.library[bookIndex].readStatus = newReadStatus;
+    readStatusButton.textContent = newReadStatus;
+
+    this.updateLocalStorage();
+  };
+
+  updateLocalStorage = () => {
+    localStorage.setItem('bookTable', bookTable.innerHTML);
+    localStorage.setItem('myLibrary', JSON.stringify(this.library));
+  };
+
+  testFunc = (test) => {
+    console.log('testfunc', test);
   };
 }
 
-function submitNewBook() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const readStatus = document.getElementById('read').checked
-    ? 'READ'
-    : 'NOT READ';
-  let isFormFilled = title.trim() !== '' && author.trim() !== '' && pages > 0;
+// function submitNewBook() {
+//   const title = document.getElementById('title').value;
+//   const author = document.getElementById('author').value;
+//   const pages = document.getElementById('pages').value;
+//   const readStatus = document.getElementById('read').checked
+//     ? 'READ'
+//     : 'NOT READ';
+//   let isFormFilled = title.trim() !== '' && author.trim() !== '' && pages > 0;
 
-  if (isFormFilled) {
-    const newBook = new Book(title, author, pages, readStatus);
-    addBookToLibrary(newBook);
-    addBookToDisplay(newBook);
-    updateLocalStorage();
-    submitForm.reset();
-    remindUserInput(title, author, pages);
-  } else {
-    remindUserInput(title, author, pages);
-    return;
-  }
-}
+//   if (isFormFilled) {
+//     const newBook = new Book(title, author, pages, readStatus);
+//     addBookToLibrary(newBook);
+//     addBookToDisplay(newBook);
+//     updateLocalStorage();
+//     submitForm.reset();
+//     remindUserInput(title, author, pages);
+//   } else {
+//     remindUserInput(title, author, pages);
+//     return;
+//   }
+// }
 
-function remindUserInput(title, author, pages) {
-  const titleReminder = document.getElementById('title-reminder');
-  const authorReminder = document.getElementById('author-reminder');
-  const pagesReminder = document.getElementById('pages-reminder');
-  const reminder = '*please fill in this field';
-  if (title.trim() === '') titleReminder.textContent = reminder;
-  else titleReminder.textContent = '';
-  if (author.trim() === '') authorReminder.textContent = reminder;
-  else authorReminder.textContent = '';
-  if (pages <= 0) pagesReminder.textContent = reminder;
-  else pagesReminder.textContent = '';
-}
+// function remindUserInput(title, author, pages) {
+//   const titleReminder = document.getElementById('title-reminder');
+//   const authorReminder = document.getElementById('author-reminder');
+//   const pagesReminder = document.getElementById('pages-reminder');
+//   const reminder = '*please fill in this field';
+//   if (title.trim() === '') titleReminder.textContent = reminder;
+//   else titleReminder.textContent = '';
+//   if (author.trim() === '') authorReminder.textContent = reminder;
+//   else authorReminder.textContent = '';
+//   if (pages <= 0) pagesReminder.textContent = reminder;
+//   else pagesReminder.textContent = '';
+// }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
+// function addBookToLibrary(book) {
+//   myLibrary.push(book);
+// }
 
-function addBookToDisplay(book) {
-  const newTableRow = document.createElement('tr');
-  newTableRow.id = book.title;
-  bookTable.appendChild(newTableRow);
+// function addBookToDisplay(book) {
+//   const newTableRow = document.createElement('tr');
+//   newTableRow.id = book.title;
+//   bookTable.appendChild(newTableRow);
 
-  newTableRow.innerHTML += '<td>' + book.title + '</td>';
-  newTableRow.innerHTML += '<td>' + book.author + '</td>';
-  newTableRow.innerHTML += '<td>' + book.pages + '</td>';
-  newTableRow.appendChild(getReadStatusTD(book));
-  newTableRow.appendChild(getDeleteTD(book));
-}
+//   newTableRow.innerHTML += '<td>' + book.title + '</td>';
+//   newTableRow.innerHTML += '<td>' + book.author + '</td>';
+//   newTableRow.innerHTML += '<td>' + book.pages + '</td>';
+//   newTableRow.appendChild(getReadStatusTD(book));
+//   newTableRow.appendChild(getDeleteTD(book));
+// }
 
-function getDeleteTD(book) {
-  const tdElement = document.createElement('td');
-  const deleteButton = document.createElement('button');
-  deleteButton.id = 'delete-' + book.title;
-  deleteButton.className = 'delete';
-  deleteButton.textContent = 'DELETE';
-  deleteButton.addEventListener('click', deleteBook);
-  tdElement.appendChild(deleteButton);
-  return tdElement;
-}
+// function getDeleteTD(book) {
+//   const tdElement = document.createElement('td');
+//   const deleteButton = document.createElement('button');
+//   deleteButton.id = 'delete-' + book.title;
+//   deleteButton.className = 'delete';
+//   deleteButton.textContent = 'DELETE';
+//   deleteButton.addEventListener('click', deleteBook);
+//   tdElement.appendChild(deleteButton);
+//   return tdElement;
+// }
 
-function getReadStatusTD(book) {
-  const readStatusTD = document.createElement('td');
-  const readStatusButton = document.createElement('button');
-  readStatusButton.textContent = book.readStatus;
-  readStatusButton.id = 'read-status-' + book.title;
-  readStatusButton.className = 'read-status';
-  readStatusButton.addEventListener('click', changeReadStatus);
-  readStatusTD.appendChild(readStatusButton);
-  return readStatusTD;
-}
+// function getReadStatusTD(book) {
+//   const readStatusTD = document.createElement('td');
+//   const readStatusButton = document.createElement('button');
+//   readStatusButton.textContent = book.readStatus;
+//   readStatusButton.id = 'read-status-' + book.title;
+//   readStatusButton.className = 'read-status';
+//   readStatusButton.addEventListener('click', changeReadStatus);
+//   readStatusTD.appendChild(readStatusButton);
+//   return readStatusTD;
+// }
 
-function deleteBook(e) {
-  const title = e.target.id.slice(7);
-  myLibrary = myLibrary.filter((book) => book.title != title);
-  document.getElementById(title).remove();
-  updateLocalStorage();
-}
+// function deleteBook(e) {
+//   const title = e.target.id.slice(7);
+//   myLibrary = myLibrary.filter((book) => book.title != title);
+//   document.getElementById(title).remove();
+//   updateLocalStorage();
+// }
 
-// change the read status in both the dislays and arrays
-function changeReadStatus(e) {
-  const title = e.target.id.slice(12);
-  const currentReadStatus = e.target.innerText;
-  const changedReadStatus = currentReadStatus === 'READ'? 'NOT READ' : 'READ';
-  const readStatusButton = document.getElementById(e.target.id);
-  const bookIndex = myLibrary.findIndex((book) => book.title === title);
-  
-  myLibrary[bookIndex].readStatus = changedReadStatus;
-  readStatusButton.textContent = changedReadStatus;
+// function changeReadStatus(e) {
+//   const title = e.target.id.slice(12);
+//   const currentReadStatus = e.target.innerText;
+//   const changedReadStatus = currentReadStatus === 'READ'? 'NOT READ' : 'READ';
+//   const readStatusButton = document.getElementById(e.target.id);
+//   const bookIndex = myLibrary.findIndex((book) => book.title === title);
 
-  updateLocalStorage();
-}
+//   myLibrary[bookIndex].readStatus = changedReadStatus;
+//   readStatusButton.textContent = changedReadStatus;
 
-function updateLocalStorage() {
-  localStorage.setItem('bookTable', bookTable.innerHTML);
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
+//   updateLocalStorage();
+// }
 
-function testFunc(test) {
-  console.log('testfunc', test);
-}
+// function updateLocalStorage() {
+//   localStorage.setItem('bookTable', bookTable.innerHTML);
+//   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+// }
+
+// function testFunc(test) {
+//   console.log('testfunc', test);
+// }
 
 const bookTable = document.getElementById('book-table');
 const submitForm = document.getElementById('submit-form');
-
-let myLibrary = [];
-
 const exampleBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'READ');
-addBookToLibrary(exampleBook);
-addBookToDisplay(exampleBook);
+
+const myLibrary = new Library([]);
+myLibrary.addBookToLibrary(exampleBook);
+myLibrary.addBookToDisplay(exampleBook);
+
+// let myLibrary = [];
+// addBookToLibrary(exampleBook);
+// addBookToDisplay(exampleBook);
 
 let savedBookTable = localStorage.getItem('bookTable');
 let savedLibrary = localStorage.getItem('myLibrary');
 if (savedBookTable) {
   bookTable.innerHTML = savedBookTable;
-  myLibrary = JSON.parse(savedLibrary);
+  myLibrary.library = JSON.parse(savedLibrary);
 }
 
 const submitButton = document.getElementById('submit-button');
-submitButton.addEventListener('click', submitNewBook);
+submitButton.addEventListener('click', myLibrary.submitNewBook);
 
 const readStatusButtons = document.querySelectorAll('button.read-status');
 readStatusButtons.forEach((button) =>
-  button.addEventListener('click', changeReadStatus)
+  button.addEventListener('click', myLibrary.changeReadStatus)
 );
 
 const deleteButons = document.querySelectorAll('button.delete');
-deleteButons.forEach((button) => button.addEventListener('click', deleteBook));
+deleteButons.forEach((button) =>
+  button.addEventListener('click', myLibrary.deleteBook)
+);
 
 /* To-do list
  */
